58362de58a62c5ed4b6f71d4faa706f2e7db3ca1
junior <junior@zipcodes-MacBook-Pro-5.local>
2021-12-08 08:36:13 -0500

    almost done with part 3

--- answers/part3.sql
+++ answers/part3.sql
@@ -12,9 +12,17 @@ JOIN pokemon_trainer ON pokemons.id = pokemon_trainer.pokemon_id
 WHERE trainerID = 303;
 
 #How many pokemon have a secondary type Poison?
+SELECT count(pokemons.name) AS number_of_pokemon, types.name AS type
+FROM pokemons JOIN types ON pokemons.secondary_type = types.id WHERE types.name = 'Poison';
 
 #What are all the primary types and how many pokemon have that type?
+SELECT count(pokemons.name) AS number_of_pokemon, types.name AS type
+FROM pokemons JOIN types ON pokemons.primary_type = types.id GROUP BY types.name;
 
 #How many pokemon at level 100 does each trainer with at least one level 100 pokemone have? (Hint: your query should not display a trainer?
+SELECT count(pokemons.name) AS number_of_pokemon, pokemon_trainer.trainerID
+FROM pokemons JOIN pokemon_trainer ON pokemons.id = pokemon_trainer.pokemon_id
+WHERE pokemon_trainer.pokelevel = 100 GROUP BY pokemon_trainer.trainerID;
 
 #How many pokemon only belong to one trainer and no other?
+
